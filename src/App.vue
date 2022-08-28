0138a589bcb79c3d21e27e9f4aacd13066d62ce6
zhangsiming <1194510899@qq.com>
2022-08-28 11:22:13 +0800

    two

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
-      <router-link></router-link>
-      <p>黑马头条</p>
-    <router-view/>
+    <router-view />
+    <p class="iconfont icon-shipin">黑马头条</p>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="success">成功按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
   </div>
 </template>
 
 <style lang="less">
-
 </style>
